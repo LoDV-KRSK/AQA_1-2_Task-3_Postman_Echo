ed71675f59085e52abf497414f8f2c8906d0d593
lodv-krsk <dloshenov@ya.ru>
2022-03-22 21:04:52 +0700

    added tests

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,37 @@
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldReturnSomeData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+
+    @Test
+    void shouldReturnTestCheck() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("test check")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("test check"))
+        ;
+    }
+}
+
